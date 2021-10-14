1e93cb7131a7178698ff239d526cc1cda8d36ad2
pfrueh1 <fruehpaul@gmail.com>
2021-10-14 12:50:27 -0400

    added inquirer

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
+//   fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-
-
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('portfolio complete! Check out index.html to see the output!')
-  })
+//     console.log('portfolio complete! Check out index.html to see the output!')
+//   })
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
